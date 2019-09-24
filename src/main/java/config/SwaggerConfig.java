2cbb0fea6aec3e0871a48856640c3ab5e5ffc074
effortmanagement <winsupersentai@gmail.com>
2019-09-24 23:42:23 +0700

    no message

--- src/main/java/config/SwaggerConfig.java
+++ src/main/java/config/SwaggerConfig.java
@@ -16,7 +16,8 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .paths(PathSelectors.ant("/api/*"))
                 .build();
     }
+
 }
