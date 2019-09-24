eb7f15cd3bd7de06f5d252aba003778fc97ea8da
effortmanagement <winsupersentai@gmail.com>
2019-09-24 23:12:11 +0700

    Init Project

---
+++ src/main/java/com/restful/api/model/Project.java
@@ -0,0 +1,130 @@
+package com.restful.api.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "project")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdDate"}, allowGetters = true)
+public class Project implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_id")
+    private int id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Column(name = "description")
+    private String desc;
+
+    @NotBlank
+    private String status;
+
+    @Temporal(TemporalType.DATE)
+    @CreatedDate
+    private Date createdDate;
+
+    @Column(name = "planned_start_date")
+    @Temporal(TemporalType.DATE)
+    private Date plannedStartDate;
+
+    @Temporal(TemporalType.DATE)
+    private Date plannedEndDate;
+
+    @Temporal(TemporalType.DATE)
+    private Date actualStartDate;
+
+    @Temporal(TemporalType.DATE)
+    private Date actualEndDate;
+    
+    private int pmId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getPlannedStartDate() {
+        return plannedStartDate;
+    }
+
+    public void setPlannedStartDate(Date plannedStartDate) {
+        this.plannedStartDate = plannedStartDate;
+    }
+
+    public Date getPlannedEndDate() {
+        return plannedEndDate;
+    }
+
+    public void setPlannedEndDate(Date plannedEndDate) {
+        this.plannedEndDate = plannedEndDate;
+    }
+
+    public Date getActualStartDate() {
+        return actualStartDate;
+    }
+
+    public void setActualStartDate(Date actualStartDate) {
+        this.actualStartDate = actualStartDate;
+    }
+
+    public Date getActualEndDate() {
+        return actualEndDate;
+    }
+
+    public void setActualEndDate(Date actualEndDate) {
+        this.actualEndDate = actualEndDate;
+    }
+
+    public int getPmId() {
+        return pmId;
+    }
+
+    public void setPmId(int pmId) {
+        this.pmId = pmId;
+    }
+}
