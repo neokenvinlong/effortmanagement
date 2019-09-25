15715eb0841d7b15b2ffcb13975579949b46ec23
effortmanagement <winsupersentai@gmail.com>
2019-09-25 19:04:58 +0700

    config swagger 3

--- src/main/java/com/restful/api/EffortManagementApiApplication.java
+++ src/main/java/com/restful/api/EffortManagementApiApplication.java
@@ -1,6 +1,7 @@
 package com.restful.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +9,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @Configuration
+@EnableSwagger2
 public class EffortManagementApiApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(EffortManagementApiApplication.class, args);
