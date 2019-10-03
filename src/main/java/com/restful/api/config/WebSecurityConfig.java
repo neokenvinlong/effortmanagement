112d76a6835cf4be7effe6349474699f84b512a1
effortmanagement <winsupersentai@gmail.com>
2019-10-03 17:41:03 +0700

    config swagger

--- src/main/java/com/restful/api/config/WebSecurityConfig.java
+++ src/main/java/com/restful/api/config/WebSecurityConfig.java
@@ -52,6 +52,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
                 .authorizeRequests().antMatchers("/authenticate").permitAll().
+                and().authorizeRequests().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**").permitAll().
                 // all other requests need to be authenticated
                         anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
